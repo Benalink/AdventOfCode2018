c4e744f9d2766d4297985a71203824fe3b391bd5
Ben Alink <ben.alink@insitetrack.com>
2018-12-05 07:00:22 +0000

    Day 2 solved

---
+++ AdventOfCode2018Tests/Day2Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+using AdventOfCode2018;
+
+namespace AdventOfCode2018Tests
+{
+    public class Day2Tests
+    {
+        [Fact]
+        public void Part1()
+        {
+            IEnumerable<string> input = File.ReadAllLines($"{AppContext.BaseDirectory}/Inputs/Day2.txt");
+
+            int result = Solvers.Day2.Part1Solver(input);
+
+            Assert.Equal(8398, result);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            IEnumerable<string> input = File.ReadAllLines($"{AppContext.BaseDirectory}/Inputs/Day2.txt");
+
+            string result = Solvers.Day2.Part2Solver(input);
+
+            Assert.Equal("hhvsdkatysmiqjxunezgwcdpr", result);
+        }
+    }
+}
