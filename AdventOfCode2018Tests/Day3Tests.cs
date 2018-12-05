0460c1b186773f69849e6c714384e619664f80f4
Ben Alink <Benalink@live.co.uk>
2018-12-05 10:54:36 +0000

    Day 3 solved

---
+++ AdventOfCode2018Tests/Day3Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+using AdventOfCode2018;
+
+namespace AdventOfCode2018Tests
+{
+    public class Day3Tests
+    {
+        [Fact]
+        public void Part1()
+        {
+            IEnumerable<string> input = File.ReadAllLines($"{AppContext.BaseDirectory}/Inputs/Day3.txt");
+
+            int result = Solvers.Day3.Part1Solver(input);
+
+            Assert.Equal(104241, result);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            IEnumerable<string> input = File.ReadAllLines($"{AppContext.BaseDirectory}/Inputs/Day3.txt");
+
+            int result = Solvers.Day3.Part2Solver(input);
+
+            Assert.Equal(806, result);
+        }
+    }
+}
